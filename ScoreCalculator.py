012ab0426ee80198238e847e33e867d402b1d37f
eim5178 <eim5178@psu.edu>
2020-10-06 20:56:29 +0000

    Score calculator

---
+++ ScoreCalculator.py
@@ -0,0 +1,62 @@
+#This program calculates the final score
+
+#The total maneuverability score is 150 possible points
+def maneuverability_course_scoring(N):
+  n = int(input("Enter nth place where 1 is the fastest team: "))
+  fastest_time_score = 150*((N+2)-n)/(N+1)
+  return fastest_time_score
+
+#The total maximum range course lap scoring is 100 possible points
+def max_range_course_lap_scoring(N):
+  n = int(input("Enter nth place where 1 is the team with the most laps: "))
+  maximum_range_course_score = 100*((N+2)-n)/(N+1)
+  return maximum_range_course_score
+#The total maximum payload fraction scoring is 50
+def payload_fraction_scoring(N):
+  payload_weight = int(input("Enter payload weight:"))
+  aircraft_wo_payload = int(input("Enter aircraft weight without payload: "))
+  payload_fraction = payload_weight/aircraft_wo_payload
+  n = int(input("Enter nth place where 1 is the highest payload fraction: "))
+  maximum_payload_fraction_scoring = 50*((N+2)-n)/(N+1)
+  return maximum_payload_fraction_scoring 
+# 25 points are awarded for successful completion of the bonus
+def bonus_scoring():
+  bonus = input("Successfully complete bonus? (Y or N)")
+  if bonus == 'Y' or bonus == 'y':
+    return 25
+  else:
+    return 0
+#This allows to write into a txt file to compile and compare results
+def write_into_txt(total_score,maneuverability_scoring,max_range_lap_scoring,payload_fract_scoring,bonus_score,total_range):
+  compile = input("Would you like to export these scores into a txt file to save them? (Y or N)")
+  if compile == 'Y' or compile =='y':
+    total_score = str(total_score)
+    maneuverability_scoring = str(maneuverability_scoring)
+    max_range_lap_scoring = str(max_range_lap_scoring)
+    payload_fract_scoring = str(payload_fract_scoring)
+    bonus_score = str(bonus_score)
+    total_range = str(total_range)
+    Scorescompiled = open("Scorescompiled.txt","w")
+    Scorescompiled.write(f"The total calculated score is " + total_score +"\n")
+    Scorescompiled.write(f"The maneuverability score is " + maneuverability_scoring+"\n")
+    Scorescompiled.write(f"The total range score is " + total_range+"\n")
+    Scorescompiled.write(f"The range course lap score is " + max_range_lap_scoring +"\n")
+    Scorescompiled.write(f"The payload fraction score is " + payload_fract_scoring +"\n")
+def run():
+  N = int(input("Enter the number of teams: "))
+  maneuverability_scoring = maneuverability_course_scoring(N)
+  max_range_lap_scoring = max_range_course_lap_scoring(N)
+  payload_fract_scoring = payload_fraction_scoring(N)
+  bonus_score = bonus_scoring()
+  total_score = maneuverability_scoring + max_range_lap_scoring + payload_fract_scoring + bonus_score
+  print(f"The total calculated score is {total_score}")
+  print(f"The maneuverability score is {maneuverability_scoring}")
+  total_range = max_range_lap_scoring + payload_fract_scoring
+  print(f"The total range score is {total_range}")
+  print(f"The range course lap score is {max_range_lap_scoring}")
+  print(f"The payload fraction score is {payload_fract_scoring}")
+  write_into_txt(total_score,maneuverability_scoring,max_range_lap_scoring,payload_fract_scoring,bonus_score,total_range)
+
+if __name__ == "__main__":
+  run()
+
